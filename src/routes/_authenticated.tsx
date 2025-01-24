52507dd3ec571ebed3220d1125c038196eabc26b
Yukendhiran <yukendhiranelango@gmail.com>
2025-01-24 13:47:03 +0530

    tailwind 4 migration

--- src/routes/_authenticated.tsx
+++ src/routes/_authenticated.tsx
@@ -46,7 +46,7 @@ function Home() {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
